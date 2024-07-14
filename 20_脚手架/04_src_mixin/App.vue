c06983df656ae32264ee819f01acae40c3397db8
liuyang <liuyang123233@pwrd.com>
2024-07-14 18:42:54 +0800

    mixin

---
+++ 20_脚手架/04_src_mixin/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <School />
+  <Student />
+</template>
+
+<script>
+import School from './components/School.vue'
+import Student from './components/Student.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Student,
+    School
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 20_脚手架/04_src_mixin/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2 @click="showName">学校名称: {{ name }}</h2>
+    <h2>学校地址: {{ address }}</h2>
+  </div>
+</template>
+
+<script>
+
+// 引入一个混合
+import {mixin} from '../mixin'
+
+export default {
+  name: "School",
+  data() {
+    return {
+      name: "学校名字",
+      address: "通州",
+    };
+  },
+  mixins: [mixin]
+};
+
+</script>
