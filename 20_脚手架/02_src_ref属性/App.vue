4f650383331ea663259c4592213d6b3c5c02f7c7
liuyang <liuyang123233@pwrd.com>
2024-07-14 14:50:19 +0800

    02_src_ref属性

---
+++ 20_脚手架/02_src_ref属性/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1 v-text="msg" ref="title"></h1>
+  <button ref="btn" @click="showDOM">点我输出上方的DOM元素</button>
+  <School ref="sch" />
+</template>
+
+<script>
+import School from './components/School.vue'
+
+export default {
+  name: 'App',
+  components: {
+    School
+  },
+  data() {
+    return {
+      msg: '欢迎学习'
+    }
+  },
+  methods: {
+    showDOM() {
+      console.log(this.$refs.title) // 真实DOM
+      console.log(this.$refs.btn)   // 真实DOM
+      console.log(this.$refs.sch)   // ViewComponent实例的代理对象
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 20_脚手架/02_src_ref属性/main.js
@@ -0,0 +1,4 @@
+import { createApp, Vue} from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
