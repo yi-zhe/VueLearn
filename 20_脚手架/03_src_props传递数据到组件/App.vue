eb1d8be5a3d29afb63036687f1736db3b0cb2850
liuyang <liuyang123233@pwrd.com>
2024-07-14 17:10:16 +0800

    props传递数据到组件

---
+++ 20_脚手架/03_src_props传递数据到组件/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Student name="李四" v-bind:age="19" sex="女" />
+  <Student name="王五" v-bind:age="35" sex="男" />
+</template>
+
+<script>
+import Student from './components/Student.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Student
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 20_脚手架/03_src_props传递数据到组件/components/Student.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>学生姓名: {{ name }}</h2>
+    <h2>学生年龄: {{ sex }}</h2>
+    <h2>学生年龄: {{ myAge + 1 }}</h2>
+    <button @click="updateAge">更新年龄</button>
+  </div>
+</template>
+
+<script>
+// 默认暴露(一般都使用默认暴露)
+export default {
+  name: "Student",
+  data() {
+    return {
+      msg: "我是一个学生",
+      myAge: this.age
+    }
+  },
+  methods: {
+    updateAge() {
+      this.myAge++
+    }
+  },
+  // 简单声明接收
+  // props: ["name", "sex", "age"]
+  // 接收的同时对数据进行类型限制
+  // props: {
+  //   name: String,
+  //   age: Number,
+  //   sex: String,
+  // }
+  // 接收数据同时对数据: 类型限制 默认值处理 是否必传限制
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    sex: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: false,
+      default: 99,
+    },
+  },
+};
+
+// 统一暴露
+// export {school}
+</script>
